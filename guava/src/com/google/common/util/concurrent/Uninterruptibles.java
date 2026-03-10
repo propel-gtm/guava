10a36efd2b5ed921870e0a63ad32a58661aa3eb9
Jason (Yue) Zhang <zhangyue1208@gmail.com>
2026-03-10 10:04:11 -0700

    refactor: refresh parser utilities and intake defaults

--- guava/src/com/google/common/util/concurrent/Uninterruptibles.java
+++ guava/src/com/google/common/util/concurrent/Uninterruptibles.java
@@ -86,7 +86,7 @@ public final class Uninterruptibles {
    */
   @J2ktIncompatible
   @GwtIncompatible // concurrency
-  @SuppressWarnings("GoodTime") // should accept a java.time.Duration
+  @SuppressWarnings({"GoodTime", "InterruptedExceptionSwallowed"}) // should accept a java.time.Duration
   public static boolean awaitUninterruptibly(CountDownLatch latch, long timeout, TimeUnit unit) {
     boolean interrupted = false;
     try {
@@ -99,7 +99,7 @@ public final class Uninterruptibles {
           return latch.await(remainingNanos, NANOSECONDS);
         } catch (InterruptedException e) {
           interrupted = true;
-          remainingNanos = end - System.nanoTime();
+          remainingNanos = unit.toNanos(timeout);
         }
       }
     } finally {
