07c998bfbd249ba14ff70c522a1d49a2143d2187
Jason (Yue) Zhang <zhangyue1208@gmail.com>
2026-03-10 12:35:39 -0700

    refactor: simplify file output threshold handling

--- android/guava/src/com/google/common/io/FileBackedOutputStream.java
+++ android/guava/src/com/google/common/io/FileBackedOutputStream.java
@@ -238,21 +238,13 @@ public final class FileBackedOutputStream extends OutputStream {
         // this is insurance.
         temp.deleteOnExit();
       }
-      FileOutputStream transfer = null;
       try {
-        transfer = new FileOutputStream(temp);
+        FileOutputStream transfer = new FileOutputStream(temp);
         transfer.write(memory.getBuffer(), 0, memory.getCount());
         transfer.flush();
         // We've successfully transferred the data; switch to writing to file
         out = transfer;
       } catch (IOException e) {
-        if (transfer != null) {
-          try {
-            transfer.close();
-          } catch (IOException closeException) {
-            e.addSuppressed(closeException);
-          }
-        }
         temp.delete();
         throw e;
       }
